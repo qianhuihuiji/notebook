d8eb5018eaa271cc801b010068269890779b7487
meiyiming <1025926969@qq.com>
2019-07-30 00:03:35 +0800

    algorithms:page-77

---
+++ Algorithms/Chapter-1/1.3/codes/Stats.java
@@ -0,0 +1,27 @@
+public class Stats
+{
+    public static void main(String[] args)
+    {
+        Bag<Double> numbers = new Bag<Double>();
+
+        while(!StdIn.isEmpty())
+            numbers.add(StdIn.readDouble());
+
+        int N = numbers.size();
+
+        double sum = 0.0;
+        for (double x : numbers)
+            sum += x;
+
+        double mean = sum/N;
+
+        sum = 0.0;
+        for (double x : numbers)
+            sum += (x - mean)*(x - mean);
+
+        double std = Math.sqrt(sum/(N-1));
+
+        StdOut.printf("Mean: %.2f\n", mean);
+        StdOut.printf("Std dev: %.2f\n", std);
+    }
+}
